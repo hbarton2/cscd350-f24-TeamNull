c8a09dd30d98ad4982cda6ab5b4e886a317d0bf2
ColtonKnopik <ColtonKnopik@gmail.com>
2024-10-11 20:49:43 -0700

    Basic framework for help class

---
+++ src/main/java/proj/TeamNull/UMLdevkit/Help.java
@@ -0,0 +1,32 @@
+package proj.TeamNull.UMLdevkit;
+
+public class Help {
+
+    public String MethodHelp(){
+        return null;    //add,remove,rename information
+    }
+
+    public String FieldHelp(){
+        return null;    //add,remove,rename information
+    }
+
+    public String ClassHelp(){
+        return null;    //add,remove,rename information
+    }
+
+    public String SaveLoadHelp(){
+        return null;    //how to save and load
+    }
+
+    public String ParameterHelp(){
+        return null;    //add,remove,rename information
+    }
+
+    public String DisplayHelp(){
+        return null;    //how to understand the display
+    }
+
+    public String GeneralHelp(){
+        return null;    //how to use the application
+    }
+}
