7f3f29cd5e667e56990eab6e3ea4a88abe0442a9
steeley21 <ksteele5@ewu.edu>
2024-10-13 10:07:53 -0700

    Minor fixes to param tests

--- src/test/java/proj/TeamNull/UMLdevkkit/UMLComponentTests/UMLParameterTest.java
+++ src/test/java/proj/TeamNull/UMLdevkkit/UMLComponentTests/UMLParameterTest.java
@@ -1,6 +1,7 @@
 package proj.TeamNull.UMLdevkkit.UMLComponentTests;
 
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
 import proj.TeamNull.UMLdevkit.UMLComponent.UMLParameter;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -14,9 +15,14 @@ public class UMLParameterTest<module> {
   private final int TIMEOUT = 1000;
   private UMLParameter umlParameter;
 
+
+  public void setUp() {
+    umlParameter = new UMLParameter("param");
+  }
+
   @Test (timeout = TIMEOUT)
   public void testAdd() {
-
+    setUp();
     UMLParameter parameter = new UMLParameter("int");
 
     // Test that add operation throws an exception
@@ -25,7 +31,7 @@ public class UMLParameterTest<module> {
 
   @Test (timeout = TIMEOUT)
   public void testRemove() {
-
+    setUp();
     UMLParameter parameter = new UMLParameter("int");
 
     // Test that remove operation throws an exception
@@ -34,7 +40,7 @@ public class UMLParameterTest<module> {
 
   @Test (timeout = TIMEOUT)
   public void testGetChild() {
-
+    setUp();
     // Test that getChild operation throws an exception
     assertThrows(UnsupportedOperationException.class, () -> umlParameter.getChild("child"));
   }
