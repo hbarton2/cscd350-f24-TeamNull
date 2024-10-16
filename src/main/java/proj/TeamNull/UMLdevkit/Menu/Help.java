5a6f5c5d661468a0b6465a70ecf12299e18662d7
Colton Knopik <120224879+ColtonKnopik@users.noreply.github.com>
2024-10-15 19:32:57 -0700

    Help Menu (#56)
    * Refactored UMLClass to consolidate relationships into components arrayList. Added Parameters to UMLField and UMLParameter. Added setNewName method into UMLClass. Filled out logic for isValidName method.
    
    * Refactored UMLClass to consolidate relationships into components arrayList. Added Parameters to UMLField and UMLParameter. Added setNewName method into UMLClass. Filled out logic for isValidName method.
    
    * Refactored UMLClass to consolidate relationships into components arrayList. Added Parameters to UMLField and UMLParameter. Added setNewName method into UMLClass. Filled out logic for isValidName method.
    
    * Added help menu and feature to process user input to properly use the menu
    
    * Added print information into the help menu. Added method call for the help menu into the main menu

--- src/main/java/proj/TeamNull/UMLdevkit/Menu/Help.java
+++ src/main/java/proj/TeamNull/UMLdevkit/Menu/Help.java
@@ -29,30 +29,52 @@ public class Help {
     }
 
 
-    public String getHelp(HelpType type) {
+    public static String getHelp(HelpType type) {
         if (type == null) {
             return "Invalid help type.";
         }
 
         switch (type) {
             case METHOD:
-                return "Method help info.";
+                return "To access any method commands you must be in the Method Menu\n" +
+                        " "+
+                        "\nAdd: Enter a valid class name of the class you wish to add to, then enter a valid method name and parameters"+
+                        "\nRemove: Enter the class name of the method you wish to remove, then enter method name" +
+                        "\nRename: This function is in development";
             case FIELD:
-                return "Field help info.";
+                return "Fields in UML represent attributes of a class. Each field has a name and a type, " +
+                        " and no two fields in the same class can have the same name."+
+                        "\n"+
+                        "\n Add: Enter class name of the class you wish to add to, enter valid field name and type"+
+                        "\n Remove: Enter class name of the class you wish to remove from, enter field name"+
+                        "\n Rename: This function is in development";
             case PARAMETER:
-                return "Parameter help info.";
+                return "Parameters in UML represent inputs to methods. Each parameter has a name and a type, " +
+                        "and no two parameters in the same method can have the same name." +
+                        "\n" +
+                        "\n Add: Enter class name and the method name where you want to add parameter, enter name and type"+
+                        "\n Remove: Enter class name and the method name where you want to remove parameter, enter parameter name"+
+                        "\n Rename: This function is in development";
             case CLASS:
-                return "Class help info.";
+                return "Classes in UML represent blueprints for objects. Classes can have fields, methods, and relationships. "
+                        + "No two classes can have the same name.\n"+
+                        "\n Add: Enter unique class name and select if you wish to add any methods/parameters"+
+                        "\n Remove: Enter name of class you wish to remove"+
+                        "\n Rename: Enter class name you wish to rename, enter the new name you want to change it to";
             case DISPLAY:
-                return "Display help info.";
+                return "Displays in UML help visualize the structure and relationships between classes, "
+                        + "including their fields and methods.\n"+
+                        "\n Display Single: enter class name of the class you want to display"+
+                        "\n Display All: Displays all current classes and relationship between them";
             case GENERAL:
-                return "General help info.";
+                return "UML (Unified Modeling Language) is a standardized visual language used to describe and model software systems. "
+                        + "It helps in representing the structure and behavior of systems.";
             default:
                 return "Invalid help type.";
         }
     }
 
-    public void printHelpMenu() {
+    public static void printHelpMenu() {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("+-----------------------------+");
@@ -67,12 +89,15 @@ public class Help {
         System.out.println("| 7. Exit                     |");
         System.out.println("+-----------------------------+");
         System.out.print("Enter your choice: ");
-        processHelpMenuInput(sc.nextInt());
+        processHelpMenuInput();
 
 
     }
 
-    private void processHelpMenuInput(int choice) {
+    private static void processHelpMenuInput() {
+        Scanner sc = new Scanner(System.in);
+
+        int choice = sc.nextInt();
         HelpType type = HelpType.fromInt(choice);
         String helpInfo = getHelp(type);
         System.out.println(helpInfo);
