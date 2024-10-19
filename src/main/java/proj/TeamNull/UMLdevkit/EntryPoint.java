a7e9565ff97ed747a973c0f3c84611d7ce375896
Delinquent-Pointer <aperturescience68k@live.com>
2024-10-19 10:02:26 -0700

    v1.1.35

--- src/main/java/proj/TeamNull/UMLdevkit/EntryPoint.java
+++ src/main/java/proj/TeamNull/UMLdevkit/EntryPoint.java
@@ -44,18 +44,6 @@ public class EntryPoint extends Application {
     //    launch(); // <-- confused what this is doing! -kate, this is how to start JavaScenes -
     // Jimmy
 
-    /*
-     * Sample code I've written to test out the add utility.
-     * It literally just creates an object, throws it into the UMLClass.add() and then prints it out.
-     * This is just test code, so feel free to remove it later.(In fact, PLEASE remove it later!!!)
-     * */
 
-    UMLClass myClass = new UMLClass("Steve");
-    System.out.println(myClass);
-    UMLField field = new UMLField("int", "age");
-    myClass.add(field);
-    System.out.println(myClass);
-
-    System.exit(0);
   }
 }
