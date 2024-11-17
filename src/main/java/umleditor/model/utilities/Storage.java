5d9fde2b6955600617d0c5c5c883540e63da1bb2
Shane Moore <102933628+sMooreEWU@users.noreply.github.com>
2024-11-17 11:34:43 -0800

    Re-adding what I have added in 'feature/Shane-addDestRelation', this time into the correct project structure. (#206)

--- src/main/java/umleditor/model/utilities/Storage.java
+++ src/main/java/umleditor/model/utilities/Storage.java
@@ -1,7 +1,10 @@
 package umleditor.model.utilities;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import umleditor.model.uml.UMLClass;
+import umleditor.model.uml.UMLRelationship;
+import umleditor.model.uml.UMLRelationshipType;
 
 /**
  * Singleton Storage class contains the collection of all UML class objects the user creates
@@ -22,11 +25,20 @@ public class Storage {
   // single instance of the Storage. Must be declared volatile for double-check locking works correctly.
   private static volatile Storage instance;
 
+
+  /**
+   *  ArrayList for all Class relations.
+   * */
+  private static ArrayList<UMLRelationship> relationships;
+
   // HashMap to store UML classes by their name
   private static HashMap<String, UMLClass> umlClasses;
 
   // Private Constructor
-  private Storage() {umlClasses = new HashMap<>();}
+  private Storage() {
+    umlClasses = new HashMap<>();
+    relationships = new ArrayList<>();
+  }
 
 
   /**
@@ -50,6 +62,13 @@ public class Storage {
   // Method to return all UML classes
   public HashMap<String, UMLClass> getUMLClasses() {
     return umlClasses;
+  }
+
+  /**
+   * Getter for the List of relationships.
+   * */
+  public ArrayList<UMLRelationship> getRelationships() {
+    return relationships;
   }
 
   // Method to set UML classes (for loading saved progress)
@@ -91,6 +110,129 @@ public class Storage {
   public void clearUMLClasses() {
     umlClasses.clear();  // Clear the map of UML classes
     System.out.println("All UML classes have been cleared.");
+  }
+
+  /**
+   * Sets the List of relationships to a new List.
+   * @param relationships is the new List to be loaded in.
+   * */
+  public static void setRelationships(ArrayList<UMLRelationship> relationships) {
+    Storage.relationships = relationships;
+  }
+
+  /**
+   * Adds a relation to the relation ArrayList.
+   * @param relation is the UMl relationship to be added to the List.
+   * */
+  public void addRelation(UMLRelationship relation){
+    if (relationships.isEmpty()) {
+      System.out.println("Adding relationship " + relation.getType() + " to class " + relation.getSource() + ".");
+      relationships.add(relation);
+      return;
+    } else if (findRelation(relation.getSource(), relation.getType(), relation.getDestination()) != -1) {
+      System.out.println("No duplicate entries allowed, please try again");
+      return;
+    }
+
+    System.out.println("Updating relationship in class " + relation.getSource() + " to " + relation.getType() + ".");
+    relationships.add(relation);
+  }
+
+  /**
+   * method that clears the 'relationships' List.
+   * */
+  public void removeAllRelationship() {
+    if (!relationships.isEmpty()) {
+      relationships.clear();
+      System.out.println("Removed all relationships.");
+    } else {
+      System.out.println("Error: No relationship exists to remove.");
+    }
+  }
+
+  /**
+   * A function to remove a *SPECIFIC* relation tuple.
+   * @param src is the source class argument.
+   * @param type is the relationship type argument.
+   * @param dest is the destination class argument.
+   * TODO: Un-stub method.
+   * */
+  public UMLRelationship removeARelation(String src, UMLRelationshipType type, String dest){
+    return null;
+  }
+
+  /**
+   * Removes all entries in 'relationships' that match the given type.
+   * @param type is the relationship type to be completely removed from 'relationships'.
+   * TODO: Un-stub method.
+   * */
+  public static int removeType(UMLRelationshipType type){
+    return 0;
+  }
+
+  /**
+   * Removes all relations of a source class.
+   * @param sourceClassName is the source class that will have all it's relationships wiped.
+   * TODO: Un-stub method.
+   * */
+  public int removeAllClassRelations(String sourceClassName){
+    return 0;
+  }
+
+  /**
+   * Removes all relations referencing a class.
+   * @param className is the class to remove completely from 'relationships'.
+   * TODO: Un-stub method.
+   * */
+
+  public int removeAllReferences(String className){
+    return 0;
+  }
+
+  /**
+   * This method alters an existing relationship given a source class name
+   * @param args array of size == 2, wherein it tells us what attributes to update in the given tuple.
+   * @param sourceName the source class relationship we are changing the type & or destination in.
+   * TODO: Need to refactor the type into either a Relationship or UMLClass object.
+   *   -Also need to un-stub the method.
+   * */
+  public int alterBySource(UMLRelationship sourceName, Object[] args) {
+    return -1;
+  }
+
+  /**
+   * This method alters an existing relationship given a source class name
+   * @param args array of size == 2, wherein it tells us what attributes to update in the given tuple.
+   * @param destinationName the destination relationship we are changing the type & or source in.
+   * TODO: Need to refactor the type into either a Relationship or UMLClass object.
+   *    -Also need to un-stub the method.
+   * */
+  public int alterByDestination(UMLRelationship destinationName, Object[] args) {
+    return -1;
+  }
+
+  /**
+   * A method for finding an equivalent relation in the List.
+   * */
+  private int findRelation(String source, UMLRelationshipType type, String destination){
+
+    for (int i = 0; i < relationships.size() - 1; i++){
+      UMLRelationship temp = relationships.get(i);
+      if (temp.getSource().equals(source) && temp.getType().equals(type) && temp.getDestination().equals(destination)){
+        return i;
+      }
+    }
+
+    return -1;
+  }
+
+  /**
+   * A function for completely wiping the model,
+   *  it's mainly used for testing.
+   * */
+  public void clear(){
+    umlClasses.clear();
+    relationships.clear();
   }
 
   // Method to get a UMLClass by its name
---
+++ src/test/java/sprint3/RemoveRelationsTest.java
@@ -0,0 +1,119 @@
+package sprint3;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import umleditor.model.uml.UMLRelationship;
+import umleditor.model.uml.UMLRelationshipType;
+import umleditor.model.utilities.Storage;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * This Class is a tester for the relationship method(s)
+ * Checks for proper input/output and that we delete the
+ * proper data without corrupting the model.
+ *
+ * */
+public class RemoveRelationsTest {
+
+    /**
+     * Mock fields used for testing
+     * */
+    private static final Storage storage = Storage.getInstance();
+    private String mockSource;
+    private String mockDestination;
+    private UMLRelationship mockRelationship;
+
+    /**
+     * Set-up the environment before each test.
+     * */
+    @BeforeEach
+    public void setUp(){
+        this.mockSource = "IAmSource";
+        this.mockDestination = "IAmDestination";
+        this.mockRelationship = new UMLRelationship(mockSource, UMLRelationshipType.GENERALIZATION, mockDestination);
+        storage.addClass(mockSource);
+        storage.addClass(mockDestination);
+        storage.addRelation(mockRelationship);
+    }
+
+    /**
+     * Tear down the static storage and private fields after each test to assure we aren't having false positives.
+     * */
+    @AfterEach
+    public void tearDown(){
+        storage.clear();
+        this.mockRelationship = new UMLRelationship();
+        this.mockDestination = "";
+        this.mockSource = "";
+    }
+
+    /**
+     * Tests to see if all relationships are properly removed.
+     * */
+    @Test
+    public void testRemoveAll(){
+        storage.removeAllRelationship();
+
+        assertNotNull(storage.getRelationships(), "ArrayList 'relationships' should not be 'NULL'."); //asserts that the list is not dereferenced.
+        assertTrue(storage.getRelationships().isEmpty(), "ArrayList 'relationships' should be empty."); // asserts that list is actually empty.
+    }
+
+    /**
+     * Tests to see if specific relationships is properly removed.
+     * */
+    @Test
+    public void testRemoveX(){
+        UMLRelationship result = storage.removeARelation(mockRelationship.getSource(), mockRelationship.getType(), mockRelationship.getDestination());
+
+//        assertNotEquals(null, result, "Result should not be null for this test!"); // asserts that we actually deleted something in this test.
+//        assertTrue(result.getClass().isInstance(UMLRelationship.class), "The result should be an instance of 'UMLRelationship."); // asserts the method returns the correct type.
+//        assertTrue(result.getSource().equals(result.getSource()), "The result should have the same source class name."); // asserts the source class name was returned properly.
+//        assertTrue(result.getType().equals(result.getType()), "The result should have the relationship type."); // asserts the relationship type was returned properly.
+//        assertTrue(result.getDestination().equals(result.getDestination()), "The result should have the same destination class name."); // asserts the destination class name was returned properly.
+//        assertEquals(null, storage.removeARelation(result.getSource(), result.getType(), result.getDestination()), "Relationship tuple can not be removed again after deletion."); //asserts we aren't trying to delete non-existent data.
+    }
+
+    /**
+     * Tests to see if a specific type of relationships is properly removed.
+     * */
+    @Test
+    public void removeType(){
+        storage.removeType(mockRelationship.getType());
+
+//        assertNotEquals(null, storage.getRelationships(), "List should not be dereferenced"); // asserts list is not dereferenced.
+//        assertFalse(storage.getRelationships().contains(mockRelationship.getType()), "There should be no more instances of 'type' in 'relationships'"); //asserts all instances of given type are removed.
+//        assertEquals(-1, storage.removeType(mockRelationship.getType()), "Method should not be able to delete same relation Type again."); // asserts we aren't deleting non-existent data.
+    }
+
+    /**
+     * Tests to see if a specific class and it's relationships are properly removed.
+     * */
+    @Test
+    public void testRemoveAllClassRelations(){
+        storage.removeAllClassRelations(mockSource);
+
+//        assertNotEquals(null, storage.getRelationships(), "List should not be dereferenced"); //asserts List is not dereferenced.
+//        ArrayList<UMLRelationship> temp = storage.getRelationships();
+//        for (int i = 0; i < storage.getRelationships().size(); i++){
+//            assertFalse(temp.get(i).getSource().equals(mockSource), "There should be no tuples where the previous 'source' exists"); //asserts that there is no tuple that contains that source name.
+//        }
+//        assertEquals(-1, storage.removeAllClassRelations(mockSource), "There should be no remaining instances left in 'relationships'"); // asserts we can not delete non-existent data.
+    }
+
+    /**
+     * Tests to see if all relationships to and from a specific class are properly removed.
+     * */
+    @Test
+    public void testRemoveAllReferences(){
+        storage.removeAllReferences(mockSource);
+
+//        assertNotEquals(null, storage.getRelationships(), "List should not be dereferenced"); //asserts List is not dereferenced.
+//        assertFalse(storage.getRelationships().contains(mockSource), "No existing references should remain in List"); //asserts we've actually deleted all references to a class in List.
+//        assertEquals(-1, storage.removeAllReferences(mockSource), "Data should not be attempted to be deleted twice."); //asserts that we aren't trying to delete non-existent data.
+    }
+
+}
