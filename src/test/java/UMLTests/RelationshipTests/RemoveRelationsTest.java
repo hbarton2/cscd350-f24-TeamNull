6f6777becf41f068f47aa57aa561793d23652516
Shane Moore <102933628+sMooreEWU@users.noreply.github.com>
2024-11-30 02:34:43 -0800

    Completed the following:
    -Un-stubbed all remove functions for relations in 'Storage'.
    -Un-commented and run all tests for 'RemoveRelationsTest'.
    -Removed the 'TODO's from the removeRelation functions.

--- src/test/java/UMLTests/RelationshipTests/RemoveRelationsTest.java
+++ src/test/java/UMLTests/RelationshipTests/RemoveRelationsTest.java
@@ -7,6 +7,8 @@ import umleditor.model.uml.UMLRelationship;
 import umleditor.model.uml.UMLRelationshipType;
 import umleditor.model.utilities.Storage;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -65,14 +67,26 @@ public class RemoveRelationsTest {
      * */
     @Test
     public void testRemoveX(){
+        // Test to see if we check the src/type & dest is empty or not
+        assertEquals(null, storage.removeARelation("", mockRelationship.getType(), mockRelationship.getDestination()), "The results should be 'NULL'.");
+        assertEquals(null, storage.removeARelation(mockRelationship.getSource(), null, mockRelationship.getDestination()),"The results should be 'NULL'.");
+        assertEquals(null, storage.removeARelation("", mockRelationship.getType(), ""), "The results should be 'NULL'.");
+
+        // actually removing from the List
         UMLRelationship result = storage.removeARelation(mockRelationship.getSource(), mockRelationship.getType(), mockRelationship.getDestination());
 
-//        assertNotEquals(null, result, "Result should not be null for this test!"); // asserts that we actually deleted something in this test.
-//        assertTrue(result.getClass().isInstance(UMLRelationship.class), "The result should be an instance of 'UMLRelationship."); // asserts the method returns the correct type.
-//        assertTrue(result.getSource().equals(result.getSource()), "The result should have the same source class name."); // asserts the source class name was returned properly.
-//        assertTrue(result.getType().equals(result.getType()), "The result should have the relationship type."); // asserts the relationship type was returned properly.
-//        assertTrue(result.getDestination().equals(result.getDestination()), "The result should have the same destination class name."); // asserts the destination class name was returned properly.
-//        assertEquals(null, storage.removeARelation(result.getSource(), result.getType(), result.getDestination()), "Relationship tuple can not be removed again after deletion."); //asserts we aren't trying to delete non-existent data.
+        assertNotEquals(null, result, "Result should not be null for this test!"); // asserts that we actually deleted something in this test.
+
+        /** This is because of some weird Java behaviour. 'isInstance()' only returns true if an instantiated object is the same
+         * instance of a relationship, not if it's the same class Object. So we have to return an instance of UMLRelationship,
+         * not the class type. I know, it's stupid.
+         * */
+        assertTrue(result.getClass().isInstance(new UMLRelationship()), "The result should be an instance of 'UMLRelationship."); // asserts the method returns the correct type.
+        //The rest of the tests
+        assertTrue(result.getSource().equals(mockSource), "The result should have the same source class name."); // asserts the source class name was returned properly.
+        assertTrue(result.getType().equals(UMLRelationshipType.GENERALIZATION), "The result should have the relationship type."); // asserts the relationship type was returned properly.
+        assertTrue(result.getDestination().equals(mockDestination), "The result should have the same destination class name."); // asserts the destination class name was returned properly.
+        assertEquals(null, storage.removeARelation(result.getSource(), result.getType(), result.getDestination()), "Relationship tuple can not be removed again after deletion."); //asserts we aren't trying to delete non-existent data.
     }
 
     /**
@@ -80,11 +94,13 @@ public class RemoveRelationsTest {
      * */
     @Test
     public void removeType(){
+        assertEquals(-1, storage.removeType(null), "Return for this call should be '-1'.");
+        assertEquals(0, storage.removeType(UMLRelationshipType.INHERITANCE), "Result should be '0' either error occurred or 'Inheritance' existed when it shouldn't.");
         storage.removeType(mockRelationship.getType());
 
-//        assertNotEquals(null, storage.getRelationships(), "List should not be dereferenced"); // asserts list is not dereferenced.
-//        assertFalse(storage.getRelationships().contains(mockRelationship.getType()), "There should be no more instances of 'type' in 'relationships'"); //asserts all instances of given type are removed.
-//        assertEquals(-1, storage.removeType(mockRelationship.getType()), "Method should not be able to delete same relation Type again."); // asserts we aren't deleting non-existent data.
+        assertNotEquals(null, storage.getRelationships(), "List should not be dereferenced"); // asserts list is not dereferenced.
+        assertFalse(storage.getRelationships().contains(mockRelationship.getType()), "There should be no more instances of 'type' in 'relationships'."); //asserts all instances of given type are removed.
+        assertEquals(-1, storage.removeType(mockRelationship.getType()), "Method should not be able to delete same relation Type again."); // asserts we aren't deleting non-existent data.
     }
 
     /**
@@ -92,14 +108,23 @@ public class RemoveRelationsTest {
      * */
     @Test
     public void testRemoveAllClassRelations(){
+        assertEquals(-1, storage.removeAllClassRelations(""));
+        assertEquals(0, storage.removeAllClassRelations("Jerry"));
         storage.removeAllClassRelations(mockSource);
 
-//        assertNotEquals(null, storage.getRelationships(), "List should not be dereferenced"); //asserts List is not dereferenced.
-//        ArrayList<UMLRelationship> temp = storage.getRelationships();
-//        for (int i = 0; i < storage.getRelationships().size(); i++){
-//            assertFalse(temp.get(i).getSource().equals(mockSource), "There should be no tuples where the previous 'source' exists"); //asserts that there is no tuple that contains that source name.
-//        }
-//        assertEquals(-1, storage.removeAllClassRelations(mockSource), "There should be no remaining instances left in 'relationships'"); // asserts we can not delete non-existent data.
+        //This is here just so the loop below actually runs through the List
+        storage.addRelation(new UMLRelationship("Mori", UMLRelationshipType.INHERITANCE, "Povich"));
+        storage.addRelation(new UMLRelationship("Mori", UMLRelationshipType.ASSOCIATION, "Father"));
+
+        assertNotEquals(null, storage.getRelationships(), "List should not be dereferenced."); //asserts List is not dereferenced.
+        //Runs through the list and maks sure the original 'mock' was deleted.
+        ArrayList<UMLRelationship> temp = storage.getRelationships();
+        for (int i = 0; i < storage.getRelationships().size(); i++){
+            assertFalse(temp.get(i).getSource().equals(mockSource), "There should be no tuples where the previous 'source' exists."); //asserts that there is no tuple that contains that source name.
+        }
+
+        storage.removeAllRelationship();
+        assertEquals(-1, storage.removeAllClassRelations(mockSource), "There should be no remaining instances left in 'relationships'."); // asserts we can not delete non-existent data.
     }
 
     /**
@@ -107,11 +132,17 @@ public class RemoveRelationsTest {
      * */
     @Test
     public void testRemoveAllReferences(){
+        assertEquals(-1, storage.removeAllReferences(""), "Param can not be empty!");
+        assertEquals(0, storage.removeAllReferences("SteveHarvey"), "Class name should not exist in the mock List.");
+
+        storage.addRelation(new UMLRelationship(mockSource, UMLRelationshipType.AGGREGATION, mockDestination));
+        storage.addRelation(new UMLRelationship(mockDestination, UMLRelationshipType.DEPENDENCY, mockSource));
         storage.removeAllReferences(mockSource);
 
-//        assertNotEquals(null, storage.getRelationships(), "List should not be dereferenced"); //asserts List is not dereferenced.
-//        assertFalse(storage.getRelationships().contains(mockSource), "No existing references should remain in List"); //asserts we've actually deleted all references to a class in List.
-//        assertEquals(-1, storage.removeAllReferences(mockSource), "Data should not be attempted to be deleted twice."); //asserts that we aren't trying to delete non-existent data.
+        assertNotEquals(null, storage.getRelationships(), "List should not be dereferenced"); //asserts List is not dereferenced.
+        assertFalse(storage.getRelationships().contains(mockSource), "No existing references should remain in List"); //asserts we've actually deleted all references to a class in List.
+        assertEquals(-1, storage.removeAllReferences(mockSource), "Data should not be attempted to be deleted twice."); //asserts that we aren't trying to delete non-existent data.
     }
 
 }
+
