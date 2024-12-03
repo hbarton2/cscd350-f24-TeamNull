b0abee226d2f075cacbd9f50f80cc50bbbbc1524
steeley21 <ksteele5@ewu.edu>
2024-12-03 04:18:23 -0800

    Added Screen Capture function. Needs Gui implementation

---
+++ src/main/java/umleditor/controller/utilities/ScreenCaptureUtil.java
@@ -0,0 +1,14 @@
+package umleditor.controller.utilities;
+
+import javafx.scene.robot.Robot;
+
+/**
+ * ScreenCaptureRobot used to capture Graphical information without requiring a Scene instance.
+ */
+public class ScreenCaptureRobot {
+
+    // construct a new Robot for simulating user interactions
+    Robot robot = new Robot();
+
+    
+}
