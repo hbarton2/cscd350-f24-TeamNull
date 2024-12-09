a328bc669f4d8281a3cc8a3a241f42141095d98f
Muhammad Wahidi <mwahidi@ewu.edu>
2024-12-08 18:27:14 -0800

    Added HelpInstruction and update HelpController class and Help.fxlm

--- src/main/java/umleditor/controller/HelpController.java
+++ src/main/java/umleditor/controller/HelpController.java
@@ -1,26 +1,40 @@
 package umleditor.controller;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
-
+import javafx.scene.text.Font;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
 public class HelpController {
-
-    @FXML
-    private TextField className;
 
     @FXML
     private TextArea helpTextArea;
     @FXML
-    private TextField currentClassName;
+    private Label label;
+
+    String path = "src/main/resources/doc/HelpInstructions.txt";
 
     @FXML
-    void helpMethod(ActionEvent event) {
+    void helpMethod(ActionEvent event) throws IOException {
 
-        helpTextArea.clear();
-        helpTextArea.appendText(className.getText() + "\nkeyword to search for help");
+        helpTextArea.clear(); // Clear the existing text in the TextArea
+        helpTextArea.setStyle("-fx-text-fill: blue;");
+        helpTextArea.setFont(new Font("Bell MT", 16));
+        helpTextArea.setEditable(false);
 
-        System.out.println("Help button clicked");
+        File file = new File(path);
+        if (file.exists() && file.isFile()) {
+            try (Scanner scanner = new Scanner(file)) {
+                while (scanner.hasNextLine()) {
+                    helpTextArea.appendText(scanner.nextLine() + "\n");
+                    helpTextArea.appendText("");
+                }
+            }
+        } else {
+            helpTextArea.appendText("Error: Help instructions file not found at path: " + path + "\n");
+        }
+        System.out.println("Help section requested");
     }
 }
